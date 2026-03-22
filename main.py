511bb859ef1eab2f6cf60059a64cb8ecd5b5a781
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-03-23 01:26:57 +0900

    feature: add timestamp to log

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
